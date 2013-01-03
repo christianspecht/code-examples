76834aa4ef5f13b0cebe4778283af8f722d8b2b6
Christian Specht <christi@nspecht.de>
2013-01-03 15:54:23 +0100

    Dapper example

---
+++ DapperExample/DapperExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.OleDb;
+using Dapper;
+
+namespace DapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=test.mdb"))
+            {
+                var list = con.Query<Product>("select * from products");
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.ProductNumber + " : " + item.Description);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string ProductNumber { get; set; }
+        public string Description { get; set; }
+    }
+}
