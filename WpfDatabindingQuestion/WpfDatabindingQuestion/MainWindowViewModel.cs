21bdc1d808a748e7d42536bc24961483079fbd7a
Christian Specht <christi@nspecht.de>
2013-01-03 16:31:39 +0100

    disable GUI (button, for now) while MessageBox is showing

--- WpfDatabindingQuestion/WpfDatabindingQuestion/MainWindowViewModel.cs
+++ WpfDatabindingQuestion/WpfDatabindingQuestion/MainWindowViewModel.cs
@@ -6,17 +6,26 @@ namespace WpfDatabindingQuestion
 {
     public class MainWindowViewModel
     {
+        private bool disableGui;
+
         public ICommand MessageCommand
         {
             get
             {
-                return new RelayCommand(this.ShowMessage);
+                return new RelayCommand(this.ShowMessage, this.IsGuiEnabled);
             }
         }
 
         private void ShowMessage(object obj)
         {
+            this.disableGui = true;
             MessageBox.Show("test");
+            this.disableGui = false;
+        }
+
+        private bool IsGuiEnabled(object obj)
+        {
+            return !this.disableGui;
         }
     }
 }
