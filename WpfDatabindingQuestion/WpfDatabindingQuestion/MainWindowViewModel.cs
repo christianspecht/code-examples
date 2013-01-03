8da40e9e2664bdc7eefbc51ffe6c5c7bd8cd1980
Christian Specht <christi@nspecht.de>
2013-01-03 17:38:56 +0100

    simulate call to external device

--- WpfDatabindingQuestion/WpfDatabindingQuestion/MainWindowViewModel.cs
+++ WpfDatabindingQuestion/WpfDatabindingQuestion/MainWindowViewModel.cs
@@ -1,6 +1,6 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 using System.Windows.Input;
-using System.ComponentModel;
 
 namespace WpfDatabindingQuestion
 {
@@ -16,10 +16,27 @@ namespace WpfDatabindingQuestion
             }
         }
 
+        public ICommand DeviceCommand
+        {
+            get
+            {
+                return new RelayCommand(this.CallExternalDevice, this.IsGuiEnabled);
+            }
+        }
+
         private void ShowMessage(object obj)
         {
+            // here, the buttons are disabled while the MessageBox is open
             this.disableGui = true;
             MessageBox.Show("test");
+            this.disableGui = false;
+        }
+
+        private void CallExternalDevice(object obj)
+        {
+            // here, the buttons are NOT disabled while the app pauses
+            this.disableGui = true;
+            Thread.Sleep(3000);  // simulate call to external device (USB missile launcher) which takes a few seconds and pauses the app
             this.disableGui = false;
         }
 
